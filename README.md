307c402d7640796bc191f42c31f05f3937616681
Matt Duffield <matt.duffield@gmail.com>
2021-07-29 21:15:09 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# ss-web-components-distro
+
+This is the distribution build for `ss-web-components`.
+
+This build contains the following components:
+
+- DataGrid
+
+## Data Grid
+The `data-grid` is meant for displaying data utilizing the HTML Table with some added features for making it easy to use.
+
+## Data Grid Example
+Execute the following in order to see the DataGrid in action:
+
+```
+npm run start
+```
+
+The open your browser to `http://localhost:8081`
+
+
---
+++ index.js
@@ -0,0 +1,33 @@
+import {$} from 'ss-web-components';
+
+const userRequest = await fetch('https://randomuser.me/api/?nat=us&results=150', {method: 'get'});
+const userResults = await userRequest.json();
+const users = userResults.results;
+
+const fixedGrid = $('#fixedGrid');
+if (fixedGrid) {
+  fixedGrid.addEventListener('filter', async (e) => {
+    console.log('vm:fixedGrid filter - ', e);
+    const request = await fetch('https://randomuser.me/api/?nat=us&gender=female&results=150', {method: 'get'});
+    const results = await request.json();
+    const data = results.results;
+    fixedGrid.items = data;
+  });
+  fixedGrid.addEventListener('unlockRecord', unlockRecord);
+  fixedGrid.addEventListener('deleteRecord', deleteRecord);
+  fixedGrid.addEventListener('selectRecord', selectRecord);
+  fixedGrid.items = users;
+}
+
+function selectRecord(e) {
+  const {row, index} = e.detail;
+  console.log('vm:selectRecord', e, e.detail);
+}
+function unlockRecord(e) {
+  const {row, index} = e.detail;
+  console.log('vm:unlockRecord', e, e.detail);
+}
+function deleteRecord(e) {
+  const {row, index} = e.detail;
+  console.log('vm:deleteRecord', e, e.detail);
+}
